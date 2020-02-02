27b5138f5771f1202d016ffc7824978cac6bd7dd
rahul <arr.gee23@gmail.com>
2020-02-02 10:14:38 +0530

    migration. relative path set and commiting jars

--- src/trec/Constants.java
+++ src/trec/Constants.java
@@ -4,10 +4,10 @@ public class Constants {
 	// 594902 indexed (id not null or content title both not null)
     public static final int TOTAL_DOC_NO = 595037;
     public static final String INDEX_PATH2 = "/home/arrgee/Documents/index2";
-    public static final String INDEX_PATH = "/home/arrgee/Documents/index3";
+    public static final String INDEX_PATH = "index3/";
     public static final String INDEX_PATH1 = "/home/arrgee/Documents/index";
-    public static final String TOPIC_XML_SOURCE = "/home/arrgee/eclipse-workspace/WapoReportMining/resources/topics.xml";
-    public static final String OUTFILE_NAME = "/home/arrgee/eclipse-workspace/WapoReportMining/results/res";
+    public static final String TOPIC_XML_SOURCE = "resources/topics.xml";
+    public static final String OUTFILE_NAME = "results/res";
     
     public static final String FILE_NAME = "wapo";
     
